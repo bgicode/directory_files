6bc5321df3218bd08b4c4997a8683e0b229b8686
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-02-15 20:51:26 +0300

    code style

--- filedirectory.php
+++ filedirectory.php
@@ -1,46 +1,46 @@
 <?php
-    $path = __DIR__; //путь к папке содержимое которой нужно отобразить
-    $fileList = array_diff( scandir($path), array( '..', '.' ) ); //список файлов и папок без . и .. массив начинается с 2
-    $inerDir = array(); 
-    foreach($fileList as $value) { // переписываем массив для нумерации с 0
-        $inerDir[] = $value;
-    }
-    $fileListPath = glob($path.'/*'); // адресса всех файлов и папок в нужном директории
-    
-    function creatingArray(&$direct, $ListPath) {
-        foreach($direct as $key => $value) { // перребор и замена элементов первичного массива
-            try {
-                $newDir = array_diff( scandir($ListPath[$key]), array( '..', '.' ) ); // попытка узнать содержимое вложенной директории
-                $newInerDir = array();
-                foreach($newDir as $value2) { //восстановление нумерации вложенной директории
-                    $newInerDir[] = $value2;
-                }
-                creatingArray($newInerDir, glob($ListPath[$key].'/*')); //повтор функии для вложенной директории
-                $direct[$value] = $newInerDir; // вложенная директория добовляется как элемент
-                unset($direct[$key]); //удаление старого элемента
+$path = __DIR__; //путь к папке содержимое которой нужно отобразить
+$arFileList = array_diff( scandir($path), array( '..', '.' ) ); //список файлов и папок без . и .. массив начинается с 2
+$arInerDir = array(); 
+foreach ($arFileList as $value) { // переписываем массив для нумерации с 0
+    $arInerDir[] = $value;
+}
+$arFileListPath = glob($path . '/*'); // адресса всех файлов и папок в нужном директории
 
-            } catch (Throwable $e) { // попытка узнать содержимое элемента привела к ошибке значит это не папка
-                $direct[$value] = $value; // новый элемент файл
-                unset($direct[$key]); //удаление старого элемента 
+function СreatingArray(&$direct, $listPath) {
+    foreach ($direct as $key => $value) { // перребор и замена элементов первичного массива
+        try {
+            $arNewDir = array_diff( scandir($listPath[$key]), array( '..', '.' ) ); // попытка узнать содержимое вложенной директории
+            $arNewInerDir = array();
+            foreach ($arNewDir as $value2) { //восстановление нумерации вложенной директории
+                $arNewInerDir[] = $value2;
             }
+            СreatingArray($arNewInerDir, glob($listPath[$key] . '/*')); //повтор функии для вложенной директории
+            $direct[$value] = $arNewInerDir; // вложенная директория добовляется как элемент
+            unset($direct[$key]); //удаление старого элемента
+
+        } catch (Throwable $e) { // попытка узнать содержимое элемента привела к ошибке значит это не папка
+            $direct[$value] = $value; // новый элемент файл
+            unset($direct[$key]); //удаление старого элемента 
         }
     }
+}
 
-    creatingArray($inerDir, $fileListPath); // запуск формирования списка файлов и подиректорий
+СreatingArray($arInerDir, $arFileListPath); // запуск формирования списка файлов и подиректорий
 
-    function creatinList($array){ //стилизация полученного списка
-        foreach($array as $key => $value){
-            if (is_array($array[$key])){ // иконка папки для массивов 
-                echo '<li class="folder"><div class="folderImg"></div><div class="name">'.$key.'</div>
-                <ul class="listWrap">';
-                creatinList($value);
-                echo '</ul>
-                </li>';
-            }else{ // иконка файла для не массивов
-                echo '<li><div class="fileImg"></div><div class="name">'.$value.'</div></li>';
-            }
+function СreatinList($array) { //стилизация полученного списка
+    foreach ($array as $key => $value) {
+        if (is_array($array[$key])) { // иконка папки для массивов 
+            echo '<li class="folder"><div class="folderImg"></div><div class="name">' . $key . '</div>
+            <ul class="listWrap">';
+            СreatinList($value);
+            echo '</ul>
+            </li>';
+        } else { // иконка файла для не массивов
+            echo '<li><div class="fileImg"></div><div class="name">' . $value . '</div></li>';
         }
     }
+}
 ?>
 
 <!DOCTYPE HTML>
@@ -54,7 +54,7 @@
     <h1><?=$path?></h1>
     <?php
         echo '<div class="DirectoryMap">';
-        creatinList($inerDir);
+        СreatinList($arInerDir);
         echo '</div>'
     ?>
 </body>
